d9f8d6cd285db03ef47d56dd99e8bb1c6f316e39
pudo <friedrich@pudo.org>
2010-11-18 23:53:07 +0100

    initial checkin

---
+++ ckanext/plugins/disqus/__init__.py
@@ -0,0 +1,49 @@
+import logging
+
+from genshi.core import TEXT
+from genshi.input import HTML
+from genshi.filters import Transformer
+
+from ckan.plugins import SingletonPlugin, implements
+from ckan.plugins.interfaces import IConfigurable, IGenshiStreamFilter
+
+import html
+
+log = logging.getLogger(__name__)
+
+class Disqus(SingletonPlugin):
+    
+    implements(IConfigurable)
+    implements(IGenshiStreamFilter)
+    
+    def configure(self, config):
+        """ 
+        Called upon CKAN setup, will pass current configuration dict
+        to the plugin to read custom options. 
+        """
+        self.disqus_name = config.get('disqus.name', None)
+        if self.disqus_name is None:
+            log.warn("No disqus forum name is set. Please set 'disqus.name' in your .ini!")
+            self.disqus_name = 'ckan'
+        
+    def filter(self, stream):
+        """
+        Required to implement IGenshiStreamFilter; will apply some HTML 
+        transformations to the page currently rendered. Depends on Pylons
+        global objects, how can this be fixed without obscuring the 
+        inteface? 
+        """
+        
+        from pylons import request, tmpl_context as c 
+        routes = request.environ.get('pylons.routes_dict')
+        if routes.get('controller') == 'package' and routes.get('action') == 'read' and c.pkg.id:
+            data = {'name': self.disqus_name, 'identifier': 'pkg-' + c.pkg.id}
+            stream = stream | Transformer('body').append(HTML(html.BOTTOM_CODE % data))
+            stream = stream | Transformer('body//div[@id="comments"]').append(HTML(html.COMMENT_CODE % data))
+        
+        if routes.get('controller') == 'home' and routes.get('action') == 'index':
+            data = {'name': self.disqus_name}
+            stream = stream | Transformer('body//ul[@class="xoxo"]').append(HTML(html.LATEST_CODE % data))
+        
+        return stream
+    
---
+++ ckanext/plugins/disqus/html.py
@@ -0,0 +1,33 @@
+COMMENT_CODE = """
+ <h4>Comments</h4> 
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+  var disqus_identifier = "%(identifier)s";
+  var disqus_developer = true;
+  (function() {
+   var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+   dsq.src = 'http://%(name)s.disqus.com/embed.js';
+   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript=%(name)s">comments powered by Disqus.</a></noscript>
+"""
+
+BOTTOM_CODE = """
+<script type="text/javascript">
+var disqus_shortname = '%(name)s';
+(function () {
+  var s = document.createElement('script'); s.async = true;
+  s.src = 'http://disqus.com/forums/%(name)s/count.js';
+  (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
+</script>
+"""
+
+LATEST_CODE = """
+<li id="recentcomments" class="widget-container widget_text">
+    <h2 class="dsq-widget-title">Recent Comments</h2>
+    <div>
+    <script type="text/javascript" src="http://disqus.com/forums/%(name)s/recent_comments_widget.js?num_items=5&hide_avatars=0&avatar_size=32&excerpt_length=200"></script>
+    </div>
+</li>"""
