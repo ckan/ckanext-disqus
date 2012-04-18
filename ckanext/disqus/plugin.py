407cd59297cd37e3fafe97d3580976060cb15f32
Toby <toby.junk@gmail.com>
2012-04-18 16:46:37 +0100

    major update to discus plugin

--- ckanext/disqus/plugin.py
+++ ckanext/disqus/plugin.py
@@ -1,73 +1,131 @@
 import logging
+import os
 
-from genshi.core import TEXT
-from genshi.input import HTML
-from genshi.filters import Transformer
+from webhelpers.html.tags import literal
+from paste.deploy.converters import asbool
+from pylons import c, request
 
-from ckan.plugins import SingletonPlugin, implements
-from ckan.plugins.interfaces import IConfigurable, IGenshiStreamFilter
-from ckan.lib.helpers import url_for
+import ckan.lib.base as base
+import ckan.plugins as plugins
 
-import html
+import ckanext.disqus
+
+
+disqus_translations = {
+    'de' : 'de_inf',
+    'es' : 'es_ES',
+    'sv' : 'sv_SE',
+    'pt' : 'pt_EU',
+    'sr' : 'sr_CYRL',
+    'sr_Latn' : 'sr_LATIN',
+    'no' : 'en', # broken no translation available
+}
+
+# These are funny disqus language codes
+# all other codes are two letter language code
+##German (Formal) = de_formal
+##German (Informal) = de_inf
+##Portuguese (Brazil) = pt_BR
+##Portuguese (European) = pt_EU
+##Serbian (Cyrillic) = sr_CYRL
+##Serbian (Latin) = sr_LATIN
+##Spanish (Argentina) = es_AR
+##Spanish (Mexico) = es_MX
+##Spanish (Spain) = es_ES
+##Swedish = sv_SE
 
 log = logging.getLogger(__name__)
 
-class Disqus(SingletonPlugin):
+def configure_template_directory(config, relative_path):
+    configure_served_directory(config, relative_path, 'extra_template_paths')
+
+def configure_public_directory(config, relative_path):
+    configure_served_directory(config, relative_path, 'extra_public_paths')
+
+def configure_served_directory(config, relative_path, config_var):
+    'Configure serving of public/template directories.'
+    assert config_var in ('extra_template_paths', 'extra_public_paths')
+    this_dir = os.path.dirname(ckanext.disqus.__file__)
+    absolute_path = os.path.join(this_dir, relative_path)
+    if absolute_path not in config.get(config_var, ''):
+        if config.get(config_var):
+            config[config_var] += ',' + absolute_path
+        else:
+            config[config_var] = absolute_path
+
+class Disqus(plugins.SingletonPlugin):
     """
     Insert javascript fragments into package pages and the home page to
     allow users to view and create comments on any package.
     """
-
-    implements(IConfigurable)
-    implements(IGenshiStreamFilter)
+    plugins.implements(plugins.IConfigurable)
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     def configure(self, config):
         """
         Called upon CKAN setup, will pass current configuration dict
         to the plugin to read custom options.
         """
-        self.disqus_name = config.get('disqus.name', None)
-        self.disqus_developer = config.get('disqus.developer', 'false')
-        if self.disqus_name is None:
+        disqus_name = config.get('disqus.name', None)
+        if disqus_name is None:
             log.warn("No disqus forum name is set. Please set \
                 'disqus.name' in your .ini!")
         config['pylons.app_globals'].has_commenting = True
 
-    def filter(self, stream):
-        """
-        Required to implement IGenshiStreamFilter; will apply some HTML
-        transformations to the page currently rendered. Depends on Pylons
-        global objects, how can this be fixed without obscuring the
-        inteface?
-        """
-        from pylons import request
-        routes = request.environ.get('pylons.routes_dict')
+        disqus_developer = asbool(config.get('disqus.developer', 'false'))
+        disqus_developer = str(disqus_developer).lower()
+        # store these so available to class methods
+        self.__class__.disqus_developer = disqus_developer
+        self.__class__.disqus_name = disqus_name
+
+    def update_config(self, config):
+        # add template directory to template path
+        configure_template_directory(config, 'templates')
+
+
+    @classmethod
+    def language(cls):
+        lang = request.environ.get('CKAN_LANG')
+        if lang in disqus_translations:
+            lang = disqus_translations[lang]
+        else:
+            lang = lang[:2]
+        return lang
+
+
+    @classmethod
+    def disqus_comments(cls):
+        '''  '''
+        # we need to create an identifier
         try:
-            identifier = routes.get('controller')
+            identifier = c.controller
             if identifier == 'package':
                 identifier = 'dataset'
-            if routes.get('id'):
-                identifier += '::' + routes.get('id')
+            if c.current_package_id:
+                identifier += '::' + c.current_package_id
+            elif c.id:
+                identifier += '::' + c.id
             else:
                 # cannot make an identifier
                 identifier = ''
             # special case
-            if routes.get('action') == 'resource_read':
-                identifier = 'dataset-resource::' + routes.get('resource_id')
+            if c.action == 'resource_read':
+                identifier = 'dataset-resource::' + c.resource_id
         except:
             identifier = ''
+        data = {'identifier' : identifier,
+                'developer' : cls.disqus_developer,
+                'language' : cls.language(),
+                'disqus_shortname': cls.disqus_name,}
+        return literal(base.render('disqus-comments.html', data))
 
-        data = {'name': self.disqus_name,
-                'identifier': identifier,
-                'disqus_developer': self.disqus_developer
-            }
-        comment_code = HTML(html.COMMENT_CODE % data)
-        stream = stream | Transformer('//span[@class="insert-comment-thread"]')\
-                 .after(comment_code)
+    @classmethod
+    def disqus_recent(cls):
+        '''  '''
+        data = {'disqus_shortname': cls.disqus_name,}
+        return literal(base.render('disqus-recent.html', data))
 
-        recent = HTML(html.RECENT_COMMENTS % data)
-        stream = stream | Transformer('//span[@class="insert-comment-recent"]')\
-                 .after(recent)
-
-        return stream
-
+    def get_helpers(self):
+        return {'disqus_comments' : self.disqus_comments,
+                'disqus_recent' : self.disqus_recent,}
