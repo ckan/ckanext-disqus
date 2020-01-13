d819a038e98ac4a86c09cfe9411b3d25893ee616
calexandr <cherniavskyi.alexandr@linkdigital.com.au>
2020-01-13 17:53:01 +0200

    pylons.app_globals

--- ckanext/disqus/plugin.py
+++ ckanext/disqus/plugin.py
@@ -1,3 +1,4 @@
+from builtins import str
 import logging
 import base64
 import hashlib
@@ -60,7 +61,7 @@ class Disqus(p.SingletonPlugin):
         if disqus_name is None:
             log.warn("No disqus forum name is set. Please set \
                 'disqus.name' in your .ini!")
-        config['pylons.app_globals'].has_commenting = True
+        # config['pylons.app_globals'].has_commenting = True
 
         disqus_developer = p.toolkit.asbool(config.get('disqus.developer',
                                                        'false'))
